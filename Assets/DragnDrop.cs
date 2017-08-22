dacea2123179a19c790ca32a681d0818806c67f4
juliorkm <julioramakrsnamandoju@gmail.com>
2017-08-22 19:45:31 -0300

    Melhorias no Drag 'n Drop
    - Juntadas as duas classes (estava desnecessário);
    - Adicionado coisas ao Inspector;
    - Adicionado Sprite de círculo (Create -> Sprites -> Circle);
    - Trocado CircleCollider2D do código por Collider2D, para ficar mais
    genérico;
    - Yoda é encontrado por tag;
    - Clamp agora limita pra não atirar pra baixo ou pra trás;
    - Removido FixedUpdate (tava desnecessário), colocado dentro do
    OnTriggerStay2D mesmo.
    
    IMPORTANTE:
    - Bug de "quicar" no círculo azul ainda presente;
    - Se uma bola já arremessada volta pro círculo por algum motivo, ela
    quica nele (não sei por que!!);
    - Objetivo é fazer o 'mouseup' desativar quando não estiver dentro do
    Trigger, mas ainda não sei como.

--- Assets/DragnDrop.cs
+++ Assets/DragnDrop.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DragnDrop : MonoBehaviour {
-    private Rigidbody2D rb;
-    private CircleCollider2D cc;
+    public float clickRange = 5f;
+    public float forceConstant = 1000f;
+    public Color unclickableColor;
 
-    private CuDeArremesso cda;
+    private bool canBeDragged = true;
+    private bool isInYodaTrigger = false;
+    private SpriteRenderer sr;
+    private Rigidbody2D rb;
+    private Collider2D cc;
 
     private bool isBeingDragged = false;
 
@@ -17,16 +22,23 @@ public class DragnDrop : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-        cda = GetComponent<CuDeArremesso>();
-        yoda = GameObject.Find("yoda");
+        yoda = GameObject.FindGameObjectWithTag("yoda");
+        sr = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
-        cc = GetComponent<CircleCollider2D>();
+        cc = GetComponent<Collider2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (isBeingDragged)
-        {
+        if (transform.position.x - yoda.transform.position.x > clickRange) {
+            canBeDragged = false;
+            sr.color = unclickableColor;
+        } else {
+            canBeDragged = true;
+            sr.color = Color.white;
+        }
+
+        if (isBeingDragged) {
             Vector3 aux = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
             transform.position = new Vector3(aux.x, aux.y, transform.position.z);
@@ -42,7 +54,11 @@ public class DragnDrop : MonoBehaviour {
             //FIM USANDO CHECAGEM
 
             //USANDO CLAMP
-            transform.position = Vector3.ClampMagnitude(transform.position - yoda.transform.position, 5f);
+            var x = Mathf.Max(transform.position.x, yoda.transform.position.x);
+            var y = Mathf.Max(transform.position.y, yoda.transform.position.y);
+            transform.position = new Vector3(x, y, transform.position.z);
+
+            transform.position = Vector3.ClampMagnitude(transform.position - yoda.transform.position, clickRange);
             transform.position += yoda.transform.position;
             //FIM USANDO CLAMP
 
@@ -51,31 +67,52 @@ public class DragnDrop : MonoBehaviour {
         }
 	}
 
-    private void FixedUpdate()
-    {
-        if (mouseup && cda.isInYodaTrigger)
-        {
-            Vector3 OneDirection = transform.position - yoda.transform.position;
-            float force = Mathf.Min(1 / OneDirection.magnitude, 100);
-            rb.AddForce(OneDirection.normalized * force * 1000);
-            mouseup = false;
+    private void ThrowBall() {
+        Vector3 throwDirection = transform.position - yoda.transform.position;
+        float force = Mathf.Clamp(1 / throwDirection.magnitude, .35f, 1);
+        rb.AddForce(throwDirection.normalized * force * forceConstant);
+    }
+
+/*
+    private void FixedUpdate() {
+        if (rb.bodyType == RigidbodyType2D.Dynamic) {
+            if (mouseup && isInYodaTrigger) {
+                ThrowBall();
+                print ("caralho2");
+                mouseup = false;
+            }
+        }
+    } */
+
+    private void OnMouseDown() {
+        if (canBeDragged) {
+            isBeingDragged = true;
+            rb.bodyType = RigidbodyType2D.Kinematic;
+            cc.enabled = false;
         }
     }
 
-    private void OnMouseDown()
-    {
-        isBeingDragged = true;
-        rb.bodyType = RigidbodyType2D.Kinematic;
-        cc.enabled = false;
-    }
-
-    private void OnMouseUp()
-    {
+    private void OnMouseUp() {
         isBeingDragged = false;
         rb.bodyType = RigidbodyType2D.Dynamic;
         cc.enabled = true;
 
         mouseup = true;
         
+    }
+    private void OnTriggerStay2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("yoda")) {
+            isInYodaTrigger = true;
+            if (mouseup) {
+                ThrowBall();
+                mouseup = false;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.CompareTag("yoda")) {
+            isInYodaTrigger = false;
+        }
     }
 }
