c19d429b0b6d93d873da7a17f61a5b67e9cee04a
juliorkm <julioramakrsnamandoju@gmail.com>
2017-08-23 20:34:49 -0300

    Explosão (sem a física ainda)
    - Objetos renomeados para melhor organziação da Scene;
    - Criada co-rotina para explosão (tem um delay pós-colisão pra
    explodir);
    - Consertado problema de "inércia" quando jogava bolas (uma bola que
    estava inicialmente rolando pra esquerda teria uma velocidade inicial
    pra esquerda ao ser jogada);
    - Criado material para partícula;
    - Criado ParticleSystem como filho da bomba.

--- Assets/DragnDrop.cs
+++ Assets/DragnDrop.cs
@@ -13,8 +13,11 @@ public class DragnDrop : MonoBehaviour {
     private SpriteRenderer sr;
     private Rigidbody2D rb;
     private Collider2D cc;
+    private ParticleSystem explosion;
 
     private bool isBeingDragged = false;
+    private bool isBeingThrown = false;
+    private bool isExploding = false;
 
     private GameObject yoda;
 
@@ -26,11 +29,12 @@ public class DragnDrop : MonoBehaviour {
         sr = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         cc = GetComponent<Collider2D>();
+        explosion = GetComponentInChildren<ParticleSystem>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (transform.position.x - yoda.transform.position.x > clickRange) {
+        if (transform.position.x - yoda.transform.position.x > clickRange || (isBeingThrown && isExploding)) {
             canBeDragged = false;
             sr.color = unclickableColor;
         } else {
@@ -68,13 +72,28 @@ public class DragnDrop : MonoBehaviour {
 	}
 
     private void ThrowBall() {
+        isBeingThrown = true;
+        rb.velocity = Vector3.zero;
         Vector3 throwDirection = transform.position - yoda.transform.position;
         float force = Mathf.Clamp(1 / throwDirection.magnitude, minimumForce, maximumForce);
         rb.AddForce(throwDirection.normalized * force * forceConstant);
     }
 
+    private IEnumerator Explosion() {
+        isExploding = true;
+        yield return new WaitForSeconds(1f);
+        cc.enabled = false;
+        sr.enabled = false;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = 0;
+        explosion.Play();
+        Destroy(gameObject, explosion.main.duration);
+    } 
+
     private void OnMouseDown() {
         if (canBeDragged) {
+            isBeingThrown = false;
             isBeingDragged = true;
             rb.bodyType = RigidbodyType2D.Kinematic;
             gameObject.layer = 8;
@@ -84,7 +103,6 @@ public class DragnDrop : MonoBehaviour {
     private void OnMouseUp() {
         isBeingDragged = false;
         rb.bodyType = RigidbodyType2D.Dynamic;
-        cc.enabled = true;
 
         if (isInYodaTrigger) {
             ThrowBall();
@@ -103,5 +121,9 @@ public class DragnDrop : MonoBehaviour {
         if (other.gameObject.CompareTag("yoda")) {
             isInYodaTrigger = false;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (isBeingThrown && !isExploding) StartCoroutine(Explosion());
     }
 }
